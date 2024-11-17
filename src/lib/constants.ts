e3b3407b0f12db314c3c81972c590c84a501cbab
Karume-lab <danielkarume@outlook.com>
2024-11-17 23:19:43 +0300

    chore: :white_check_mark: good enough for use

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_COOKIE_KEY = "agri-gear-auth-cookie";
+export const SITE_COOKIE_KEY = "next-js-fullstack-rest-template-auth-cookie";
 export const PAGE_SIZE = 10;
 export const QUERY_KEYS = {
   tasks: "tasks",

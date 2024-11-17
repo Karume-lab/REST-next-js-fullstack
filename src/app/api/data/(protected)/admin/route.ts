b0666554778da529612bd8fcacde8790134960ec
Karume-lab <danielkarume@outlook.com>
2024-11-17 10:04:41 +0300

    fix: :passport_control: improve access control

--- src/app/api/data/(protected)/admin/route.ts
+++ src/app/api/data/(protected)/admin/route.ts
@@ -1,9 +1,15 @@
-import { checkAuthorization } from "@/lib/utils";
+import { validateRequest } from "@/lib/lucia";
+import { isAdminOrOwner } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const user = await checkAuthorization();
+  const { user } = await validateRequest();
 
+  if (!user || !isAdminOrOwner(user)) {
+    return new Response(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+    });
+  }
   console.log("reached");
   return NextResponse.json({ message: "Hello World" });
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,6 @@ import { cache } from "react";
 import { twMerge } from "tailwind-merge";
 import prisma from "./prisma";
 import { notFound } from "next/navigation";
-import { validateRequest } from "./lucia";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,23 +12,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export const isAdminOrOwner = (user: User, entityId?: string) => {
   return user.role === UserRole.ADMIN || user.id === entityId;
-};
-
-export const checkAuthorization = async () => {
-  const { user } = await validateRequest();
-  if (!user) {
-    return new Response(JSON.stringify({ message: "Unauthorized" }), {
-      status: 401,
-    });
-  }
-
-  if (!isAdminOrOwner(user)) {
-    return new Response(JSON.stringify({ message: "Unauthorized" }), {
-      status: 401,
-    });
-  }
-
-  return user;
 };
 
 export const getTask = cache(async (taskId: string) => {

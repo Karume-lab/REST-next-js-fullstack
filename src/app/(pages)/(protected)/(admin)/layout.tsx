9759e18ba8321d27c2b66a0c301c3626402d2a89
Karume-lab <danielkarume@outlook.com>
2024-11-17 08:10:00 +0300

    feat: :sparkles: add checking of roles in admin pages

---
+++ src/app/(pages)/(protected)/(admin)/layout.tsx
@@ -0,0 +1,25 @@
+import { validateRequest } from "@/lib/lucia";
+import { redirect } from "next/navigation";
+import { urls } from "@/lib/urls";
+import { SessionProvider } from "@/providers/SessionProvider";
+import { UserRole } from "@prisma/client";
+
+export default async function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session = await validateRequest();
+
+  if (!session.user || session.user.role !== UserRole.ADMIN) {
+    redirect(urls.PUBLIC_TASKS);
+  }
+
+  return (
+    <SessionProvider value={session}>
+      <div className="min-h-screen bg-background">
+        <main className="container mx-auto py-4">{children}</main>
+      </div>
+    </SessionProvider>
+  );
+}
